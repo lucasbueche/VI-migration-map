548cfa8f4180dcf63631b7d5e45ac540cfd67c22
corentinQui <corentin.bompard@edu.hefr.ch>
2021-12-14 20:03:55 +0100

    Version 2 of the map with centered image

--- app/script/test2.js
+++ app/script/test2.js
@@ -1,20 +1,42 @@
+function main_draw() {
+    const red_to_green_degraded = ["#ff0000",
+	"#fe4400",
+	"#f86600",
+	"#ee8200",
+	"#df9b00",
+	"#cdb200",
+	"#b6c700",
+	"#98db00",
+	"#6fed00",
+	"#00ff00"
+	];
+
 // The svg
 var svg = d3.select("svg"),
   width = +svg.attr("width"),
   height = +svg.attr("height");
 
+svg.selectAll("g").remove();
+
 // Map and projection
 var path = d3.geoPath();
 var projection = d3.geoMercator()
-  .scale(70)
+  .scale(120)
   .center([0,20])
   .translate([width/2, height/2]);
+
+// Zooming
+var svg = d3.select("#my_dataviz")
+.call(d3.zoom().on("zoom", function () {
+    svg.attr("transform", d3.event.transform)
+}))
+.append("g");
 
 // Data and color scale
 var data = d3.map();
 var colorScale = d3.scaleThreshold()
   .domain([-1000000, -10000, -100, 0, 100, 10000, 1000000])
-  .range(d3.schemeBlues[7]);
+  .range(red_to_green_degraded);
 
 // Load external data and boot
 d3.queue()
@@ -23,20 +45,47 @@ d3.queue()
   .await(ready);
 
 function ready(error, topo) {
-
-  // Draw the map
-  svg.append("g")
-    .selectAll("path")
-    .data(topo.features)
-    .enter()
-    .append("path")
-      // draw each country
-      .attr("d", d3.geoPath()
-        .projection(projection)
-      )
-      // set the color of each country
-      .attr("fill", function (d) {
-        d.total = data.get(d.id) || 0;
-        return colorScale(d.total);
-      });
+    let mouseOver = function(d) {
+        d3.selectAll(".Country")
+          .transition()
+          .duration(2)
+          .style("opacity", .5)
+        d3.select(this)
+          .transition()
+          .duration(2)
+          .style("opacity", 1)
+          .style("stroke", "black")
+      }
+    let mouseLeave = function(d) {
+    d3.selectAll(".Country")
+        .transition()
+        .duration(2)
+        .style("opacity", .8)
+    d3.select(this)
+        .transition()
+        .duration(2)
+        .style("stroke", "transparent")
     }
+    // Draw the map
+    svg.append("g")
+        .selectAll("path")
+        .data(topo.features)
+        .enter()
+        .append("path")
+        // draw each country
+        .attr("d", d3.geoPath()
+            .projection(projection)
+        )
+        // set the color of each country
+        .attr("fill", function (d) {
+            d.total = data.get(d.id) || 0;
+            return colorScale(d.total);
+        })
+        .style("stroke", "transparent")
+        .attr("class", function(d){ return "Country" } )
+        .style("opacity", .8)
+        .on("mouseover", mouseOver )
+        .on("mouseleave", mouseLeave )
+    }
+}
+main_draw();
