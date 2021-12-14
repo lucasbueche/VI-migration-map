8aaf45860ab5be4b0238e03ecd8832ff8002c137
corentinQui <corentin.bompard@edu.hefr.ch>
2021-12-14 11:38:31 +0100

    beautify code

--- app/script/test.js
+++ app/script/test.js
@@ -2,21 +2,20 @@
 /* eslint-disable no-undef */
 
 // Adding basic elements
-const body = d3.select('body');
+const body = d3.select("body");
 
 // Buttons
-const buttons = d3.select('#buttons')
+const buttons = d3.select("#buttons");
 
 // Adding svg
-const zoom = d3.zoom().scaleExtent([1, 40]).on('zoom', zoomed);
+const zoom = d3.zoom().scaleExtent([1, 40]).on("zoom", zoomed);
 const height = 650;
 const svg = d3
-  .select('#map')
-  .append('svg')
-  .attr('width', '100%')
-  .attr('height', height)
+  .select("#map")
+  .append("svg")
+  .attr("width", "100%")
+  .attr("height", height)
   .call(zoom);
-
 
 // moveToBack function: move an element from the front to the back
 d3.selection.prototype.moveToBack = function () {
@@ -41,13 +40,11 @@ const projection = d3
   .translate([0.5 * window.innerWidth, 375]);
 const pathGenerator = d3.geoPath().projection(projection);
 
-const worldMapDiasplay = svg.append('g');
-const map = worldMapDiasplay.append('g').attr('id', 'map');
-
-
+const worldMapDiasplay = svg.append("g");
+const map = worldMapDiasplay.append("g").attr("id", "map");
 
 // Creation of the world map
-d3.json('../../data/map/world_map.json').then((data) => {
+d3.json("../../data/map/world_map.json").then((data) => {
   let features = data.features;
 
   // Gathering country centroids
@@ -60,39 +57,37 @@ d3.json('../../data/map/world_map.json').then((data) => {
   // Generate map
 
   map
-    .selectAll('path')
+    .selectAll("path")
     .data(features)
     .enter()
-    .append('path')
-    .attr('class', 'map')
-    .attr('d', pathGenerator)
-    .style('fill', '#273147')
-    .style('stroke', 'rgb(0, 0, 0)')
-    .style('stroke-opacity', '0.25')
-    .on('mouseover', () => d3.select(event.target).style('fill', '#485470'))
-    .on('mouseout', () => d3.select(event.target).style('fill', '#273147'))
-    .on('click', (clicked) => {
+    .append("path")
+    .attr("class", "map")
+    .attr("d", pathGenerator)
+    .style("fill", "#273147")
+    .style("stroke", "rgb(0, 0, 0)")
+    .style("stroke-opacity", "0.25")
+    .on("mouseover", () => d3.select(event.target).style("fill", "#485470"))
+    .on("mouseout", () => d3.select(event.target).style("fill", "#273147"))
+    .on("click", (clicked) => {
       legend.moveToFront();
       generateDataDisplay(clicked, centroids);
     });
 });
 
 function generateDataDisplay(element, centroids) {
-        // Return CSV promise
-        let csv_data = getData();
-        const country = element.properties.name.split(' ').join('_');
-      
-      
-        // Call the data gathering function
-        //getMigration(csv_data, country, center, centroids);
-}      
-      
-      
+  // Return CSV promise
+  let csv_data = getData();
+  const country = element.properties.name.split(" ").join("_");
+
+  // Call the data gathering function
+  //getMigration(csv_data, country, center, centroids);
+}
+
 // Data loading function
 function getData() {
-        return d3.csv('../../data/CSV/dataset_2015.csv');
+  return d3.csv("../../data/CSV/dataset_2015.csv");
 }
-      
+
 function zoomed() {
-        worldMapDiasplay.attr('transform', d3.event.transform);
+  worldMapDiasplay.attr("transform", d3.event.transform);
 }
